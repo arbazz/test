7947f79c7fd0aee77bb1d57d9380aef1c53413b6
Arbaz <arbtouseef@gmail.com>
2022-12-12 12:46:33 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const myPlaintextPassword = "s0//P4$$w0rD";
+const someOtherPlaintextPassword = "not_bacon";
+
+const salt = bcrypt.genSaltSync(saltRounds);
+const hash = bcrypt.hashSync(myPlaintextPassword, salt);
+const re = bcrypt.compareSync(someOtherPlaintextPassword, hash); // true
+console.log(re);
